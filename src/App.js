501ee9c20a6130e0f5f98f00aad9a2cde2f57c50
Teethiti <Teethiti.ws@gmail.com>
2023-10-14 00:28:13 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [moneyInput, setMoneyInput] = useState("");
+  const [curInput, setCurInput] = useState("EUR");
+  const [curOutput, setCurOutput] = useState("USD");
+  const [moneyOutput, setMoneyOutput] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function fetchConvertMoney() {
+        setIsLoading(true);
+
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${moneyInput}&from=${curInput}&to=${curOutput}`
+        );
+        const data = await res.json();
+        setMoneyOutput(data.rates[curOutput]);
+
+        // console.log(data.rates[curOutput]);
+
+        setIsLoading(false);
+      }
+      if (curInput === curOutput) return setMoneyOutput(moneyInput);
+      fetchConvertMoney();
+    },
+    [moneyInput, curInput, curOutput]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setMoneyInput(Number(e.target.value));
+        }}
+        value={moneyInput}
+        disabled={isLoading}
+      />
+      <select
+        value={curInput}
+        onChange={(e) => setCurInput(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={curOutput}
+        onChange={(e) => setCurOutput(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {moneyOutput} {curOutput}
+      </p>
     </div>
   );
 }
-
-export default App;
